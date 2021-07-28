3e188a2450e1c308cd0c0fe56b42f00b0903d455
Ulia <demyra00@mail.ru>
2021-07-28 22:28:59 +1000

    dobavnenoaudio

--- CustomRace/Assets/Scripts/PlayerController.cs
+++ CustomRace/Assets/Scripts/PlayerController.cs
@@ -6,38 +6,75 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float angle;
+    public ParticleSystem part1;
+    public ParticleSystem part2;
+    private AudioSource AvtoSound;
+    public GameObject car;
     void Start()
     {
-        
+        AvtoSound = car.GetComponent<AudioSource>();
+    }
+    void AvtoSoundPlay()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            AvtoSound.Play();
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            AvtoSound.Pause();
+        }
+
+    }
+    void CarEffectPlay()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            part1.Play();
+            part2.Play();
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            part1.Stop();
+            part2.Stop();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos0ffset=gameObject.transform.position;
+        WASDController();
+        CarEffectPlay();
+        AvtoSoundPlay(); 
+    }
+    void WASDController()
+    {
+        Vector3 pos0ffset = gameObject.transform.position;
         Vector3 rot0ffset = Vector3.zero;
-       //Time.deltaTime
+        //Time.deltaTime
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            pos0ffset += Time.deltaTime * speed* gameObject.transform.forward;
+            pos0ffset += Time.deltaTime * speed * gameObject.transform.forward;
 
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
             pos0ffset -= Time.deltaTime * speed * gameObject.transform.forward;
         }
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            rot0ffset= new Vector3(0f, -Time.deltaTime * angle, 0f);
+            rot0ffset = new Vector3(0f, -Time.deltaTime * angle, 0f);
 
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            rot0ffset= new Vector3(0f, Time.deltaTime * angle, 0f);
+            rot0ffset = new Vector3(0f, Time.deltaTime * angle, 0f);
 
         }
         gameObject.transform.position = pos0ffset;
         gameObject.transform.Rotate(rot0ffset);
-       
+
     }
 }
+
